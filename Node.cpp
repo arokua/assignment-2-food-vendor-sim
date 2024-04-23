0b3fc2705e89b86b512e135019517118c09f3c26
arokua <trannguyenphucanh@gmail.com>
2024-04-23 23:33:31 +1000

    add sample

--- Node.cpp
+++ Node.cpp
@@ -1,8 +1,15 @@
 #include "Node.h"
 
+Node::Node(){
+
+}
 Node::Node(int data, Node* next) : data(data), next(next) {
     
 }
 
 Node::Node(Node& other) : data(other.data), next(other.next) {
+}
+
+Node::~Node(){
+    delete next;
 }
--- main.cpp
+++ main.cpp
@@ -41,40 +41,49 @@ vector<string> takeInput(int * nNodes){
 
 
 
-int main(){
+int main(int argc, char ** argv){
+    
     // assume inputs are of correct format
     //Get number of queries
-    int n=0;
-    string numInp="";
-    while (n < 1){
-        cout << "Enter number of queries:\t";
-        getline(cin,numInp);
-        n=stoi(numInp);
-    }
-    for (int i=0; i < n;i++){    //Get input
-        int * nNodes=new int(0);
-        vector<string> ins = takeInput( nNodes);
-        
-        //Take in the list of nodes value   
-        vector<int> arr; //vector that holds the node values
-        for (int i = 0; i < *nNodes; i++){
-            arr.push_back(stoi(ins[i].c_str()));
+    if (argc ==1 || argc ==3){
+        //ar
+        int n=0;
+        string numInp="";
+        if (argc==3) cout << "File names:\t" << argv[1]<<", "<< argv[2]<<endl;
+        while (n < 1){
+            cout << "Enter number of queries:\t";
+            getline(cin,numInp);
+            n=stoi(numInp);
         }
-        //Make a linked list object using above values
-        LinkedList ll(arr,*nNodes);
-        //Operations to be performed
-        string order = ins[*nNodes];
-        if (order == "AF") ll.addFront(stoi(ins[*nNodes + 1].c_str()));
-        else if (order == "AE") ll.addEnd(stoi(ins[*nNodes + 1].c_str()));
-        else if (order == "AP") ll.addAtPosition(stoi(ins[*nNodes + 1].c_str()), stoi(ins[*nNodes + 2].c_str()));
-        else if (order == "DF") ll.deleteFront();
-        else if (order == "DE") ll.deleteEnd();
-        else if (order == "DP") ll.deletePosition(stoi(ins[*nNodes + 1].c_str()));
-        else if (order == "S") ll.search(stoi(ins[*nNodes + 1].c_str()));
-        else if (order == "GI") ll.getItem(stoi(ins[*nNodes + 1].c_str()));
-        else cout << "Invalid command\n";
-        //Print out result
-        ll.printItems();
-        delete nNodes; // Free memory
+        for (int i=0; i < n;i++){    //Get input
+            int * nNodes=new int(0);
+            vector<string> ins = takeInput( nNodes);
+            
+            //Take in the list of nodes value   
+            vector<int> arr; //vector that holds the node values
+            for (int i = 0; i < *nNodes; i++){
+                arr.push_back(stoi(ins[i].c_str()));
+            }
+            //Make a linked list object using above values
+            LinkedList ll(arr,*nNodes);
+            //Operations to be performed
+            string order = ins[*nNodes];
+            if (order == "AF") ll.addFront(stoi(ins[*nNodes + 1].c_str()));
+            else if (order == "AE") ll.addEnd(stoi(ins[*nNodes + 1].c_str()));
+            else if (order == "AP") ll.addAtPosition(stoi(ins[*nNodes + 1].c_str()), stoi(ins[*nNodes + 2].c_str()));
+            else if (order == "DF") ll.deleteFront();
+            else if (order == "DE") ll.deleteEnd();
+            else if (order == "DP") ll.deletePosition(stoi(ins[*nNodes + 1].c_str()));
+            else if (order == "S") ll.search(stoi(ins[*nNodes + 1].c_str()));
+            else if (order == "GI") ll.getItem(stoi(ins[*nNodes + 1].c_str()));
+            else cout << "Invalid command\n";
+            //Print out result
+            ll.printItems();
+            delete nNodes; // Free memory
+        }
+    }else{
+        //Incorrect number of file inputs
+        cout << "Expect 2 file inputs!\n";
+        cout << "Usage: ./main coin.dat food.dat\n";
     }
 }
